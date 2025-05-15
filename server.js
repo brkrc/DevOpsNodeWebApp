b9682a8d5ee4e812fa077d9b0600e04419e1a2c3
brkrc <105289234+brkrc@users.noreply.github.com>
2025-05-15 13:12:58 +0200

    test commit after change on server.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!!');
+  res.send('Hello FS2025 DevOps Course! orucber1 managed to finish this Docker-Deployment Task!');
 });
 
 app.listen(PORT, HOST);
